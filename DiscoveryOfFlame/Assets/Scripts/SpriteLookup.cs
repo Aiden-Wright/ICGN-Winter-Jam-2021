c1205ca22ccaeb356c8b1e0d7ecc44f8180c8f45
Aiden Wright <aiden.wright@mymail.champlain.edu>
2021-02-20 14:55:38 -0500

    working on fire

---
+++ DiscoveryOfFlame/Assets/Scripts/SpriteLookup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLookup : MonoBehaviour
+{
+    public enum TerrainType
+    {
+        bush,
+        grass,
+        rocks,
+        riverH,
+        riverV
+    }
+    [System.Serializable]
+    public struct TerrainTypeSprite
+    {
+        public Sprite[] sprite;
+    }
+
+    public static Type getEnum()
+    {
+        return typeof(TerrainType);
+    }
+}
--- DiscoveryOfFlame/Assets/Scripts/Tile.cs
+++ DiscoveryOfFlame/Assets/Scripts/Tile.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public SpriteLookup.TerrainType type;
+    public Vector2 location;
+    public float hydration;
+    public bool onFire;
+    //int enums = Enum.GetNames(SpriteLookup.getEnum()).Length;
+    public SpriteLookup.TerrainTypeSprite[] sprites;
+    
+
     // Start is called before the first frame update
     void Start()
     {
