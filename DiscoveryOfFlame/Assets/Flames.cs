b11ad9315a6e55e3a172ad95d4a06d0cfb196c95
Aiden Wright <aiden.wright@mymail.champlain.edu>
2021-02-21 11:41:09 -0500

    save before push

---
+++ DiscoveryOfFlame/Assets/Flames.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flames : MonoBehaviour
+{
+    public Sprite[] flames = new Sprite[4457];
+    int spr = 0;
+    private void FixedUpdate()
+    {
+        GetComponent<SpriteRenderer>().sprite = flames[(++spr)%4457];
+        
+    }
+}
